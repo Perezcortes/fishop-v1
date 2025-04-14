7831dfb813bef5796b9b4d2beab77a56f06c47fa
José Pérez <9531447499a@gmail.com>
2025-04-14 14:41:23 -0600

    archivos finales

--- server/src/build/keys.js
+++ server/src/build/keys.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
+export default {
     database: {
-        host: 'database-fishop.cfydnegj4pay.us-east-2.rds.amazonaws.com',
-        user: 'admin',
-        password: 'root123*',
+        host: 'localhost',
+        user: 'root',
+        password: '',
         database: 'fishop',
-        port: 3306
+        port : 3306
     }
 };
--- server/src/keys.ts
+++ server/src/keys.ts
@@ -1,8 +1,8 @@
 export default {
     database: {
-        host: 'database-fishop.cfydnegj4pay.us-east-2.rds.amazonaws.com',
-        user: 'admin',
-        password: 'root123*',
+        host: 'localhost',
+        user: 'root',
+        password: '',
         database: 'fishop',
         port : 3306
     }
